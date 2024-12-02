7f9855da110b9e885a521b22dd02975fc19f9c4a
Luis Vicente <59994945+lvicenteperea@users.noreply.github.com>
2024-12-02 14:09:47 +0100

    solo comentarios y documentación

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,45 +1,6 @@
-# para versiones antiguas --> 
-
-        # from pydantic import BaseSettings
-        # class Settings(BaseSettings):
-        #     PROJECT_NAME: str = "MADRE"
-        #     '''
-        #         Usuarios de Localhost en PC terraza
-        #         DB_CONFIG = {
-        #                     'user': 'root',
-        #                     'password': 'admin',
-        #                     'host': 'localhost',
-        #                     'database': 'test',
-        #                 }
-
-        #         DB_CONFIG = {
-        #                     'user': 'luis',
-        #                     'password': 'luis.01',
-        #                     'host': 'localhost',
-        #                     'database': 'test',
-        #                 }
-        #     '''
-        #     MYSQL_DB_URL: str = "127.0.0.1"
-        #     MYSQL_DB_USER =  'test'
-        #     MYSQL_DB_PWD = 'Test.01'
-        #     MYSQL_DB_HOST = 'localhost'
-        #     MYSQL_DB_DATABASE = 'test'
-
-        #     MONGODB_URL: str = ""
-
-        #     LOG_LEVEL: str = "info"
-
-        #     class Config:
-        #         env_file = ".env"
-
-        # settings = Settings()
-
-
-
-
 # con la nueva versión de pedantic, la variables deben ser anotadas:
 from pydantic_settings import BaseSettings
-from typing import ClassVar
+# from typing import ClassVar
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "MADRE"

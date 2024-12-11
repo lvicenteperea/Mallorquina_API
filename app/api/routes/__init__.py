fc0b24b8671fc821be55ddf5cdf62161f5082124
lvicenteperea <lavp99@hotmail.com>
2024-12-11 19:25:49 +0100

    Ignorar ciertos archivos o directorios

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from app.api.routes import semillas, mallorquina #, backend, partners
+from app.api.routes import mallorquina
 
 router = APIRouter()
-router.include_router(semillas.router, tags=["Semillas"])
 router.include_router(mallorquina.router, tags=["Mallorquina"])
 # router.include_router(backend.router, prefix="/backend", tags=["backend"])
 # router.include_router(partners.router, prefix="/partners", tags=["partners"])
